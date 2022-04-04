8c19035c9d9f74dffd65b18f0b2af1ab989331e5
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2022-04-04 00:49:37 -0400

    works on chat flow css

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -42,12 +42,20 @@ export default {
 
 <template>
 	<div id="chatbox-container">
-		<div id="flow-chat">
-			<div id="message-box" v-for="messageInfo in chatLog">
-				<div>{{ messageInfo.senderName }}: {{ messageInfo.message }}</div>
+		<div id="flow-chat-wrapper">
+			<div id="flow-chat">
+				<div id="message-box" v-for="messageInfo in chatLog">
+					<span class="sender">{{ messageInfo.senderName }}:</span>
+					<span class="message-content">{{ messageInfo.message }}</span>
+				</div>
 			</div>
 		</div>
-		<input id="chat-input" v-model="chatMessage" v-on:keyup.enter="sendMessage" />
+		<input
+			id="chat-input"
+			v-model="chatMessage"
+			v-on:keyup.enter="sendMessage"
+			placeholder="Press ENTER to send..."
+		/>
 	</div>
 </template>
 
@@ -63,18 +71,62 @@ export default {
 	height: v-bind(height + borderWidth * 2 + "px");
 }
 
-#flow-chat {
+#flow-chat-wrapper {
+	overflow-x: hidden;
+	overflow-y: scroll;
 	border: 6px double blueviolet;
-	height: 620px;
+}
+
+#flow-chat {
+	margin: auto;
+	/* display: flex;
+	flex-direction: column;
+	justify-content: flex-end; */
+	height: 608px;
 }
 
 #chat-input {
 	font-family: "Raleway", sans-serif;
 	font-size: 18px;
 	outline: none;
-	border: 3px solid rgb(164, 148, 0);
+	border: 3px solid rgb(233, 210, 0);
 	display: block;
-	height: 31px;
+	height: 30px;
 	width: 100%;
+}
+
+#message-box {
+	margin-left: 6px;
+	text-align: left;
+}
+
+.sender {
+	margin: 0 6px 0 0;
+	padding: 0 2px 0 0;
+}
+
+.message-content {
+	word-wrap: break-word;
+	white-space: normal;
+}
+
+/* width */
+::-webkit-scrollbar {
+	width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+	background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+	background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+	background: #555;
 }
 </style>
